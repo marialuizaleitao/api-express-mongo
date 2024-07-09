e4e747ad7ed3d788950bf733a83dff7940d428da
Maria Leitão <malufln.99@gmail.com>
2024-07-09 17:00:32 -0300

    feat: enhance books route with POST functionality

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const books = [
- {
-  id: 1,
-  title: "Lord of the Rings",
- },
- {
-  id: 2,
-  title: "The Power of Now",
- },
+  {
+    id: 1,
+    title: "Lord of the Rings",
+  },
+  {
+    id: 2,
+    title: "The Power of Now",
+  },
 ];
 
 app.get("/", (req, res) => {
- res.status(200).send("Node.js application");
+  res.status(200).send("Node.js application");
 });
 
 app.get("/books", (req, res) => {
- res.status(200).json(books);
+  res.status(200).json(books);
+});
+
+app.post("/books", (req, res) => {
+  books.push(req.body)
+  res.status(201).send("Book registered!");
 });
 
 export default app;
