250236aca92a4013fa76409b7aa68e49ac142161
Maria Leitão <malufln.99@gmail.com>
2024-07-09 13:19:28 -0300

    feat: add books route with sample data

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+const books = [
+ {
+  id: 1,
+  title: "Lord of the Rings",
+ },
+ {
+  id: 2,
+  title: "The Power of Now",
+ },
+];
+
 app.get("/", (req, res) => {
  res.status(200).send("Node.js application");
+});
+
+app.get("/books", (req, res) => {
+ res.status(200).json(books);
 });
 
 export default app;
