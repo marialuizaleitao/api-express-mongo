f313d8485cfebe8d77d3220fa0020fdace272e86
Maria Leitão <malufln.99@gmail.com>
2024-07-10 18:42:23 -0300

    feat: add BookController method for listing a book by id and handle errors

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,17 +1,7 @@
 import book from "../models/Book.js";
+import mongoose from "mongoose";
 
 class BookController {
-  static async listBooks(req, res) {
-    try {
-      const bookList = await book.find({});
-      res.status(200).json(bookList);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: `${error.message} - request failed.` });
-    }
-  }
-
   static async createBook(req, res) {
     try {
       const newBook = await book.create(req.body);
@@ -20,6 +10,31 @@ class BookController {
       res
         .status(500)
         .json({ message: `${error.message} - error while creating book.` });
+    }
+  }
+
+  static async listBooks(req, res) {
+    try {
+      const bookList = await book.find({});
+      res.status(200).json(bookList);
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - request failed.` });
+    }
+  }
+
+  static async listBookById(req, res) {
+    try {
+      const id = req.params.id;
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid book ID." });
+      }
+      const bookFound = await book.findById(id);
+      if (!bookFound) {
+        return res.status(404).json({ message: "Book not found." });
+      }
+      res.status(200).json(bookFound);
+    } catch (error) {
+      res.status(500).json({ message: `${error.message} - book not found.` });
     }
   }
 }
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,7 +3,8 @@ import BookController from "../controllers/bookController.js";
 
 const routes = express.Router();
 
-routes.get("/books", BookController.listBooks);
 routes.post("/books", BookController.createBook);
+routes.get("/books", BookController.listBooks);
+routes.get("/books/:id", BookController.listBookById);
 
 export default routes;
